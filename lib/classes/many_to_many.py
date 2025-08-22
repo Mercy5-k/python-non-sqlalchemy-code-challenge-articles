e5d293bc298bc7cda5f2962a6e7b14598f220b6a
Mercy <kiqsmercy@gmail.com>
2025-08-22 03:00:13 +0300

    Added article and passed the author tests

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,40 +1,75 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
+        # Validate inputs
+        if not isinstance(author, Author):
+            raise Exception("author must be an Author instance")
+        if not isinstance(magazine, Magazine):
+            raise Exception("magazine must be a Magazine instance")
+        if not isinstance(title, str):
+            raise Exception("title must be a string")
+        if not (5 <= len(title) <= 50):
+            raise Exception("title must be between 5 and 50 characters")
+
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+
+        Article.all.append(self)
+
+    # Properties for author, magazine, and title
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, new_author):
+        if not isinstance(new_author, Author):
+            raise Exception("author must be an Author instance")
+        self._author = new_author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, new_magazine):
+        if not isinstance(new_magazine, Magazine):
+            raise Exception("magazine must be a Magazine instance")
+        self._magazine = new_magazine
         
 class Author:
-     def __init__(self, name):
-        if not isinstance(name, str) or len(name) == 0:
-            raise Exception("name must be a non-empty string")
+    def __init__(self, name):
+        if not isinstance(name, str) or len(name.strip()) == 0:
+            raise Exception("Author name must be a non-empty string")
         self._name = name
-        
-        @property
-        def name(self): 
-            return self._name
-            
-            @name.setter
-            def name(self, value):
-                # keep name immutable
-                pass
-                
-                def articles(self):
-                    return [article for article in Article.all() if article.author == self]
-                    pass
-                    
-                    def magazines(self):
-                        return list({article.magazine for article in self.articles()})
-                        pass
-                        
-                        def add_article(self, magazine, title):
-                            return Article(self, magazine, title)
-                            pass
-                            
-                            def topic_areas(self):
-                                categories ={magazine.category for magazine in self.magazines()}
-                                return list(categories) if categories else None
-                                pass
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        # name is immutable, so we do not allow changes
+        pass
+
+    def articles(self):
+        return [article for article in Article.all if article.author == self]
+
+    def magazines(self):
+        return list({article.magazine for article in self.articles()})
+
+    def add_article(self, magazine, title):
+        return Article(self, magazine, title)
+
+    def topic_areas(self):
+        categories = {magazine.category for magazine in self.magazines()}
+        return list(categories) if categories else None
 
 class Magazine:
     all_magazines = [] # Class variable to hold all magazine instances
